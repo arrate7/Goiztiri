bebe2d8f485d5d4fb98bf7a570eae0ec002d3161
arrate <arrateoc@gmail.com>
2018-11-06 18:50:34 +0100

    lista Femenino conseguido

---
+++ Goiztiri/Goiztiri/Controllers/JugadoresController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Goiztiri.Models;
+using GoiztiriWeb.Models;
+
+namespace Goiztiri.Controllers
+{
+    public class JugadoresController : Controller
+    {
+        private GoiztiriFContext db = new GoiztiriFContext();
+
+        // GET: Jugadores
+        public ActionResult Index()
+        {
+            return View(db.Jugadores.ToList());
+        }
+
+        public ActionResult Femenino()
+        {
+            return View(db.Jugadores.ToList());
+        }
+
+        // GET: Jugadores/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Jugador jugador = db.Jugadores.Find(id);
+            if (jugador == null)
+            {
+                return HttpNotFound();
+            }
+            return View(jugador);
+        }
+
+        // GET: Jugadores/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Jugadores/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "CodJugador,Ruta,CodEquipo,Dorsal,Nombre,Apellido,Goles,Asistencias,Demarcacion")] Jugador jugador)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Jugadores.Add(jugador);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(jugador);
+        }
+
+        // GET: Jugadores/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Jugador jugador = db.Jugadores.Find(id);
+            if (jugador == null)
+            {
+                return HttpNotFound();
+            }
+            return View(jugador);
+        }
+
+        // POST: Jugadores/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "CodJugador,Ruta,CodEquipo,Dorsal,Nombre,Apellido,Goles,Asistencias,Demarcacion")] Jugador jugador)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(jugador).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(jugador);
+        }
+
+        // GET: Jugadores/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Jugador jugador = db.Jugadores.Find(id);
+            if (jugador == null)
+            {
+                return HttpNotFound();
+            }
+            return View(jugador);
+        }
+
+        // POST: Jugadores/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Jugador jugador = db.Jugadores.Find(id);
+            db.Jugadores.Remove(jugador);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
--- Goiztiri/Goiztiri/Models/Jugador.cs
+++ Goiztiri/Goiztiri/Models/Jugador.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,12 +8,17 @@ namespace GoiztiriWeb.Models
 {
     public class Jugador
     {
+        public string Ruta { get; set; }
+        public int CodEquipo { get; set; }
+        [Key]
+        public int CodJugador{ get; set; }
         public int Dorsal { get; set; }
         public string Nombre{ get; set; }
         public string Apellido{ get; set; }
-        public int AnioInicio { get; set; }
         public int Goles { get; set; }
         public int Asistencias { get; set; }
+        public string Demarcacion{ get; set; }
+
 
     }
 }
